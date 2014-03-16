391d761448b261935be1f82813adf19ac952ad4e
andythenorth <mail@andythenorth.co.uk>
2014-03-16 21:26:54 +0000

    Change: add Buildwas and Limebreach mining trams

--- src/rosters/brit.py
+++ src/rosters/brit.py
@@ -1,12 +1,14 @@
 from roster import Roster
 
 from vehicles import broadrock
+from vehicles import buildwas
 from vehicles import buff
 from vehicles import cloud_hill
 from vehicles import california
 from vehicles import crime_rigg
 from vehicles import greenscoe
 from vehicles import griff
+from vehicles import limebreach
 from vehicles import knockdown
 from vehicles import meriden
 from vehicles import pigstick
@@ -15,7 +17,9 @@ from vehicles import stungun
 from vehicles import witch_hill
 
 roster = Roster(id = 'brit',
-                buy_menu_sort_order = ['broadrock',
+                buy_menu_sort_order = ['buildwas',
+                                       'limebreach',
+                                       'broadrock',
                                        'witch_hill',
                                        'crime_rigg',
                                        'griff',
