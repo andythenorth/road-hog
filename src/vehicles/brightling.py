553e81cc36e5e586ca2eef348714ae8c15457bad
andythenorth <mail@andythenorth.co.uk>
2014-03-22 09:28:54 +0000

    Change: 2 units for Brightling tram

--- src/vehicles/brightling.py
+++ src/vehicles/brightling.py
@@ -20,7 +20,7 @@ consist.add_unit(GeneralCargoHauler(consist = consist,
                         weight = 10,
                         capacity_freight = 50,
                         vehicle_length = 7,
-                        spriterow_num = 0))
+                        spriterow_num = 0), repeat=2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
