7a3248af53b619a262cc2976632cb5b5bb8b5ea3
andythenorth <mail@andythenorth.co.uk>
2014-03-27 12:33:43 +0000

    Codechange: update GPL notification to correct set name

--- src/render_docs.py
+++ src/render_docs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 """
-  This file is part of Iron Horse Newgrf for OpenTTD.
-  Iron Horse is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
-  Iron Horse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Iron Horse. If not, see <http://www.gnu.org/licenses/>.
+  This file is part of Road Hog Newgrf for OpenTTD.
+  Road Hog is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+  Road Hog is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Road Hog. If not, see <http://www.gnu.org/licenses/>.
 """
 print "[RENDER DOCS] render_docs.py"
 
