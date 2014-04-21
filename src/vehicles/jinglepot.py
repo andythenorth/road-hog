065981f68c05bb3411c6b2dd04eefe365939efe2
andythenorth <mail@andythenorth.co.uk>
2014-04-21 21:41:56 +0100

    Change: graphics for Jinglepot truck

--- src/vehicles/jinglepot.py
+++ src/vehicles/jinglepot.py
@@ -21,7 +21,7 @@ consist.add_unit(GeneralCargoHauler(consist = consist,
                         weight = 5,
                         capacity_freight = 15,
                         vehicle_length = 7,
-                        spriterow_num = 0))
+                        spriterow_num = 1))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
