e1dd492319e1aa71c344c473eb190f93e503f499
andythenorth <mail@andythenorth.co.uk>
2014-04-24 20:30:41 +0100

    Change: improved Stancliffe Foundry Hauler

--- src/vehicles/stancliffe.py
+++ src/vehicles/stancliffe.py
@@ -6,7 +6,7 @@ consist = EngineConsist(id = 'stancliffe',
               title = 'Stancliffe [Foundry Hauler]',
               roadveh_flag_tram = True,
               replacement_id = '-none',
-              power = 200,
+              power = 250,
               speed = 27,
               vehicle_life = 40,
               intro_date = 1928,
@@ -15,15 +15,15 @@ consist = EngineConsist(id = 'stancliffe',
 consist.add_unit(FoundryHauler(consist = consist,
                         weight = 20,
                         capacity_freight = 0,
-                        vehicle_length = 7,
+                        vehicle_length = 3,
                         visual_effect = 'VISUAL_EFFECT_ELECTRIC',
                         spriterow_num = 0))
 
 consist.add_unit(FoundryHauler(consist = consist,
                         weight = 5,
                         capacity_freight = 20,
-                        vehicle_length = 7,
-                        spriterow_num = 0), repeat = 5)
+                        vehicle_length = 4,
+                        spriterow_num = 1), repeat = 5)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
