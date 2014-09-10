5dd17564b11a84134b3e902b4729633a4193fec8
andythenorth <mail@andythenorth.co.uk>
2014-09-10 21:07:17 +0100

    Feature: graphics for Shotover Livestock Tram

--- src/vehicles/shotover.py
+++ src/vehicles/shotover.py
@@ -9,15 +9,14 @@ consist = EngineConsist(id = 'shotover',
               power = 120,
               speed = 35,
               vehicle_life = 40,
-              intro_date = 1903,
-              graphics_status = '')
+              intro_date = 1903)
 
 consist.add_unit(LivestockHauler(consist = consist,
                         weight = 12,
                         capacity_freight = 20,
                         vehicle_length = 7,
                         visual_effect = 'VISUAL_EFFECT_ELECTRIC',
-                        spriterow_num = 0), repeat = 2)
+                        spriterow_num = 0))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
