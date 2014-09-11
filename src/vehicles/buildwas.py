b00c4480741ef1e4a942379bd6a86243e8c6575c
andythenorth <mail@andythenorth.co.uk>
2014-09-11 08:17:37 +0100

    Feature: graphics for Buildwas Tram

--- src/vehicles/buildwas.py
+++ src/vehicles/buildwas.py
@@ -14,21 +14,15 @@ consist = EngineConsist(id = 'buildwas',
 consist.add_unit(GeneralCargoHauler(consist = consist,
                         weight = 30,
                         capacity_freight = 0,
-                        vehicle_length = 7,
+                        vehicle_length = 6,
                         visual_effect = 'VISUAL_EFFECT_STEAM',
                         spriterow_num = 0))
 
 consist.add_unit(GeneralCargoHauler(consist = consist,
                         weight = 2,
                         capacity_freight = 9,
-                        vehicle_length = 7,
-                        spriterow_num = 0), repeat=8)
-
-consist.add_unit(GeneralCargoHauler(consist = consist,
-                        weight = 4,
-                        capacity_freight = 0,
-                        vehicle_length = 7,
-                        spriterow_num = 0))
+                        vehicle_length = 4,
+                        spriterow_num = 2), repeat=8)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
