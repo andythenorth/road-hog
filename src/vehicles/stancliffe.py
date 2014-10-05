bb8efbb3bd00b8123cf04c3c9bd8c91530b0bfaf
andythenorth <mail@andythenorth.co.uk>
2014-10-05 22:02:05 +0100

    Change: adjust stats for Stancliffe Foundry Tram

--- src/vehicles/stancliffe.py
+++ src/vehicles/stancliffe.py
@@ -7,22 +7,21 @@ consist = EngineConsist(id = 'stancliffe',
               roadveh_flag_tram = True,
               replacement_id = '-none',
               power = 250,
-              speed = 25,
               vehicle_life = 40,
               intro_date = 1900)
 
 consist.add_unit(FoundryHauler(consist = consist,
                         weight = 20,
-                        capacity = 0,
+                        capacity = 5,
                         vehicle_length = 3,
                         visual_effect = 'VISUAL_EFFECT_ELECTRIC',
                         spriterow_num = 0))
 
 consist.add_unit(FoundryHauler(consist = consist,
                         weight = 5,
-                        capacity = 20,
+                        capacity = 15,
                         vehicle_length = 4,
-                        spriterow_num = 1), repeat = 5)
+                        spriterow_num = 1), repeat = 3)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
