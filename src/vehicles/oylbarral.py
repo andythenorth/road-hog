f810df3b1c9d8a2803c985191a96907bf95f2db2
andythenorth <mail@andythenorth.co.uk>
2014-10-14 16:39:07 +0100

    Feature: sprites for Oylbarral tanker tram

--- src/vehicles/oylbarral.py
+++ src/vehicles/oylbarral.py
@@ -6,16 +6,22 @@ consist = EngineConsist(id = 'oylbarral',
               title = 'Oylbarral [Tanker Tram]',
               roadveh_flag_tram = True,
               replacement_id = '-none',
-              power = 150,
+              power = 300,
               vehicle_life = 40,
-              intro_date = 1993)
+              intro_date = 1905)
 
 consist.add_unit(Tanker(consist = consist,
-                        weight = 20,
-                        capacity = 20,
-                        vehicle_length = 7,
+                        weight = 10,
+                        capacity = 15,
+                        vehicle_length = 6,
                         visual_effect = 'VISUAL_EFFECT_DIESEL',
-                        spriterow_num = 0), repeat=2)
+                        spriterow_num = 0))
+
+consist.add_unit(Tanker(consist = consist,
+                        weight = 5,
+                        capacity = 15,
+                        vehicle_length = 5,
+                        spriterow_num = 1), repeat=1)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
