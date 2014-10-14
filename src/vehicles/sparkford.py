f11e915d8c93aa3c4d9913755f664b88dea0ca11
andythenorth <mail@andythenorth.co.uk>
2014-10-14 17:32:36 +0100

    Fix: stats for Sparkford reefer tram

--- src/vehicles/sparkford.py
+++ src/vehicles/sparkford.py
@@ -6,21 +6,15 @@ consist = EngineConsist(id = 'sparkford',
               title = 'Sparkford [Reefer Tram]',
               roadveh_flag_tram = True,
               replacement_id = '-none',
-              power = 450,
+              power = 200,
               vehicle_life = 40,
-              intro_date = 1982)
+              intro_date = 1915)
 
 consist.add_unit(RefrigeratedHauler(consist = consist,
-                        weight = 8,
-                        capacity = 0,
+                        weight = 14,
+                        capacity = 30,
                         vehicle_length = 7,
                         visual_effect = 'VISUAL_EFFECT_DIESEL',
-                        spriterow_num = 0))
-
-consist.add_unit(RefrigeratedHauler(consist = consist,
-                        weight = 10,
-                        capacity = 36,
-                        vehicle_length = 7,
                         spriterow_num = 0))
 
 consist.add_model_variant(intro_date=0,
