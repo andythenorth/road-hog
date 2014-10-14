181f6e0ce21e42a40f71b8b8ee081ba7f863909b
andythenorth <mail@andythenorth.co.uk>
2014-10-14 15:38:29 +0100

    Feature: sprites for Coalhall tram

--- src/vehicles/coalhall.py
+++ src/vehicles/coalhall.py
@@ -14,7 +14,7 @@ consist = EngineConsist(id = 'coalhall',
 
 consist.add_unit(MiningHauler(consist = consist,
                         weight = 30,
-                        capacity = 0,
+                        capacity = 10,
                         vehicle_length = 6,
                         visual_effect = 'VISUAL_EFFECT_ELECTRIC',
                         spriterow_num = 0))
@@ -23,7 +23,7 @@ consist.add_unit(MiningHauler(consist = consist,
                         weight = 2,
                         capacity = 10,
                         vehicle_length = 5,
-                        spriterow_num = 2), repeat=5)
+                        spriterow_num = 1), repeat=4)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
