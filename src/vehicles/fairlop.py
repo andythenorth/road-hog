e4c624a3834b82cc962487c2943895d3fbbc42d9
andythenorth <mail@andythenorth.co.uk>
2014-10-17 18:15:04 +0100

    Fix: correct length for Fairlop tram

--- src/vehicles/fairlop.py
+++ src/vehicles/fairlop.py
@@ -13,7 +13,7 @@ consist = EngineConsist(id = 'fairlop',
 consist.add_unit(PaxHauler(consist = consist,
                         weight = 12,
                         capacity = 40,
-                        vehicle_length = 7,
+                        vehicle_length = 6,
                         effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 12'],
                         spriterow_num = 0))
 
