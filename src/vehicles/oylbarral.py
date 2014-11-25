d21b43677890a34e1873461d427d692c10afb614
andythenorth <mail@andythenorth.co.uk>
2014-11-25 10:13:53 +0000

    Change: adjust Oylbarral tram capacity down, it was creating problems with truck progression

--- src/vehicles/oylbarral.py
+++ src/vehicles/oylbarral.py
@@ -10,16 +10,19 @@ consist = EngineConsist(id = 'oylbarral',
               vehicle_life = 40,
               intro_date = 1903)
 
+# Don't increase capacity for brit tanker tram, it messes up progression of the trucks
+# Got bulk oil?  Use a pipeline, ship or train instead of RVs.
+# Other Road Hog rosters will have bigger RV tankers (both tram + truck).
 consist.add_unit(Tanker(consist = consist,
                         weight = 10,
-                        capacity = 20,
+                        capacity = 15,
                         vehicle_length = 6,
                         effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
                         spriterow_num = 0))
 
 consist.add_unit(Tanker(consist = consist,
                         weight = 5,
-                        capacity = 20,
+                        capacity = 51,
                         vehicle_length = 5,
                         spriterow_num = 1), repeat=1)
 
