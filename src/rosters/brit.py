a981793468631fc507cecfd7820a581434cb7c16
andythenorth <mail@andythenorth.co.uk>
2015-02-18 19:44:17 +0000

    Codechange: remove unused roster.title attribute

--- src/rosters/brit.py
+++ src/rosters/brit.py
@@ -65,7 +65,6 @@ from vehicles import (acton,
                       wookey)
 
 roster = Roster(id = 'brit',
-                title = 'Brit',
                 truck_speeds = {0: 20, 1900: 25, 1920: 40, 1940: 55, 1960: 70, 1980: 80},
                 tram_speeds = {0: 20, 1895: 35, 1940: 50, 1980: 65},
                 vehicles = [ladycross,
--- src/rosters/wasteland.py
+++ src/rosters/wasteland.py
@@ -3,7 +3,6 @@ from roster import Roster
 from vehicles import wastelander
 
 roster = Roster(id = 'wasteland',
-                title = 'Wasteland',
                 truck_speeds = {0: 20, 1900: 25, 1920: 40, 1940: 55, 1960: 70, 1980: 80},
                 tram_speeds = {0: 20, 1895: 35, 1940: 50, 1980: 65},
                 vehicles = [wastelander])
