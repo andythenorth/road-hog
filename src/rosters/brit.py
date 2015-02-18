5197151b0dc6d9baa04ad659b2bd1e492312cfbe
andythenorth <mail@andythenorth.co.uk>
2015-02-18 19:45:53 +0000

    Codechange: stub in roster.numeric_id

--- src/rosters/brit.py
+++ src/rosters/brit.py
@@ -65,6 +65,7 @@ from vehicles import (acton,
                       wookey)
 
 roster = Roster(id = 'brit',
+                numeric_id = 1,
                 truck_speeds = {0: 20, 1900: 25, 1920: 40, 1940: 55, 1960: 70, 1980: 80},
                 tram_speeds = {0: 20, 1895: 35, 1940: 50, 1980: 65},
                 vehicles = [ladycross,
