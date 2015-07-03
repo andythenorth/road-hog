6be20284be2e64327a1495566b8a03283169928c
andythenorth <mail@andythenorth.co.uk>
2015-07-03 22:41:04 +0100

    Change: stub in more open trucks

---
+++ src/vehicles/capo.py
@@ -0,0 +1,28 @@
+import global_constants
+from road_vehicle import EngineConsist, OpenHauler
+
+consist = EngineConsist(id = 'capo',
+              base_numeric_id = 680,
+              title = 'Capo [Open Truck]',
+              replacement_id = '-none',
+              power = 120,
+              vehicle_life = 40,
+              intro_date = 1980)
+
+consist.add_unit(OpenHauler(consist = consist,
+                        weight = 10,
+                        capacity = 10,
+                        vehicle_length = 5,
+                        effect_spawn_model = 'EFFECT_SPAWN_MODEL_STEAM',
+                        effects = ['EFFECT_SPRITE_STEAM, -3, 0, 12'],
+                        spriterow_num = 0))
+
+consist.add_unit(OpenHauler(consist = consist,
+                        weight = 5,
+                        capacity = 15,
+                        vehicle_length = 5,
+                        spriterow_num = 1))
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
