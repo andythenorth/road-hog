08198d2beeb03cc3ed56d1b68d1a827c28be4f68
andythenorth <mail@andythenorth.co.uk>
2015-07-18 15:20:15 +0100

    Change: rear cab for Oylbarral tanker tram

--- src/vehicles/oylbarral.py
+++ src/vehicles/oylbarral.py
@@ -21,7 +21,14 @@ consist.add_unit(Tanker(consist = consist,
                         weight = 5,
                         capacity = 30,
                         vehicle_length = 4,
-                        spriterow_num = 1), repeat=2)
+                        spriterow_num = 1))
+
+consist.add_unit(Tanker(consist = consist,
+                        weight = 10,
+                        capacity = 30,
+                        vehicle_length = 6,
+                        effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
+                        spriterow_num = 2))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
