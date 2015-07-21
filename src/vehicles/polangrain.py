90e4739619404bfe4deae6860e22a4e37ea2784c
andythenorth <mail@andythenorth.co.uk>
2015-07-21 17:22:29 +0100

    Change: Polangrain covered hopper tram

---
+++ src/vehicles/polangrain.py
@@ -0,0 +1,22 @@
+import global_constants
+from road_vehicle import EngineConsist, BulkPowderHauler
+
+consist = EngineConsist(id = 'polangrain',
+              base_numeric_id = 790,
+              title = 'Polangrain [Covered Hopper Tram]',
+              roadveh_flag_tram = True,
+              replacement_id = '-none',
+              power = 200,
+              vehicle_life = 40,
+              intro_date = 1900)
+
+consist.add_unit(BulkPowderHauler(consist = consist,
+                        weight = 12,
+                        capacity = 40,
+                        vehicle_length = 8,
+                        effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
+                        spriterow_num = 0), repeat=2)
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
