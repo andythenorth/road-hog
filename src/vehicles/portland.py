c62eb6a85c9db548d014b7dd68342c122da02647
andythenorth <mail@andythenorth.co.uk>
2015-07-22 22:37:43 +0100

    Change: adjust Portland tram

--- src/vehicles/portland.py
+++ src/vehicles/portland.py
@@ -12,16 +12,16 @@ consist = EngineConsist(id = 'portland',
 
 consist.add_unit(OpenHauler(consist = consist,
                         weight = 12,
-                        capacity = 18,
+                        capacity = 24,
                         vehicle_length = 7,
                         effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
                         spriterow_num = 0))
 
 consist.add_unit(OpenHauler(consist = consist,
                         weight = 4,
-                        capacity = 18,
+                        capacity = 24,
                         vehicle_length = 3,
-                        spriterow_num = 2), repeat=3)
+                        spriterow_num = 2), repeat=2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
