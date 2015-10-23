ce3e55caaea3cde6c5f35fa8b1b0a3a13565ef43
andythenorth <mail@andythenorth.co.uk>
2015-10-23 17:32:23 +0100

    Feature: add Cowsleigh livestock truck

---
+++ src/vehicles/cowsleigh.py
@@ -0,0 +1,28 @@
+import global_constants
+from road_vehicle import EngineConsist, LivestockHauler
+
+consist = EngineConsist(id = 'cowsleigh',
+              base_numeric_id = 900,
+              title = 'Cowsleigh [Livestock Truck]',
+              replacement_id = '-none',
+              power = 150,
+              speed_dibble = 'plodding', # cows don't like to go *too* fast, apply speed penalty :P
+              vehicle_life = 40,
+              intro_date = 1911)
+
+consist.add_unit(LivestockHauler(consist = consist,
+                        weight = 10,
+                        capacity = 13,
+                        vehicle_length = 6,
+                        visual_effect = 'VISUAL_EFFECT_DIESEL',
+                        spriterow_num = 0))
+
+consist.add_unit(LivestockHauler(consist = consist,
+                        weight = 10,
+                        capacity = 12,
+                        vehicle_length = 5,
+                        spriterow_num = 1))
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
