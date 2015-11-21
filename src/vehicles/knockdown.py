2c7a3460a51f0f7b0bd4dadf8626dbbaa1763792
andythenorth <mail@andythenorth.co.uk>
2015-11-21 11:10:52 +0000

    Codechange: adjust knockdown logging truck spritesheet order

--- src/vehicles/knockdown.py
+++ src/vehicles/knockdown.py
@@ -21,7 +21,7 @@ consist.add_unit(LogHauler(consist = consist,
                         weight = 7,
                         capacity = 30,
                         vehicle_length = 6,
-                        spriterow_num = 1))
+                        spriterow_num = 3))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
