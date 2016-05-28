6b5ed4d087c42f07f63d3a60c41b767e7de4a50a
andythenorth <mail@andythenorth.co.uk>
2016-05-28 15:57:59 +0100

    Change: (WIP) Witch Hill Mining Truck has a trailer

--- src/vehicles/witch_hill.py
+++ src/vehicles/witch_hill.py
@@ -3,8 +3,8 @@ import graphics_processor.utils as graphics_utils
 from road_vehicle import RVConsist, DumpHauler
 
 graphics_processors = graphics_utils.get_bulk_cargo_processors(template='witch_hill_template.png',
-                                              copy_block_top_offsets = [10],
-                                              paste_top_offset = 10)
+                                              copy_block_top_offsets = [40],
+                                              paste_top_offset = 40)
 
 consist = RVConsist(id = 'witch_hill',
               base_numeric_id = 500,
@@ -18,12 +18,25 @@ consist = RVConsist(id = 'witch_hill',
 
 consist.add_unit(DumpHauler(consist = consist,
                         weight = 60,
+                        capacity = 0,
+                        vehicle_length = 3,
+                        semi_truck_shift_offset_jank = 3,
+                        effects = ['EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, 1, 10', 'EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, -1, 10'],
+                        always_use_same_spriterow = True,
+                        spriterow_num = 0))
+
+consist.add_unit(DumpHauler(consist = consist,
+                        weight = 0, # put the weight on the truck to compensate for lack of TE when loaded
                         capacity = 120,
                         vehicle_length = 7,
-                        effects = ['EFFECT_SPRITE_DIESEL, -3, 0, 13'],
-                        spriterow_num = 0))
+                        spriterow_adjust = {'multiplier': 0, 'offset': 1}))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0,
                        graphics_processor=graphics_processors[0])
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=1,
+                       graphics_processor=graphics_processors[1])
