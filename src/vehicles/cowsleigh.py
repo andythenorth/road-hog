15ead07b677224c75579788c67266eb0a9e6f1c6
andythenorth <mail@andythenorth.co.uk>
2016-06-21 20:08:21 +0100

    Change: improved Cowsleigh and Pigstick livestock trucks

--- src/vehicles/cowsleigh.py
+++ src/vehicles/cowsleigh.py
@@ -10,15 +10,14 @@ consist = RVConsist(vehicle_type = LivestockHauler,
                 intro_date = 1911)
 
 consist.add_unit(weight = 10,
-                capacity = 0,
-                vehicle_length = 4,
+                capacity = 14,
+                vehicle_length = 6,
                 effect_spawn_model = 'EFFECT_SPAWN_MODEL_STEAM',
                 effects = ['EFFECT_SPRITE_STEAM, -5, 0, 12'])
 
 consist.add_unit(weight = 8,
-                capacity = 12,
-                vehicle_length = 4,
-                repeat = 2)
+                capacity = 10,
+                vehicle_length = 4)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
--- src/vehicles/pigstick.py
+++ src/vehicles/pigstick.py
@@ -10,13 +10,13 @@ consist = RVConsist(vehicle_type = LivestockHauler,
                 intro_date = 1941)
 
 consist.add_unit(weight = 10,
-                capacity = 15,
+                capacity = 20,
                 vehicle_length = 6,
                 visual_effect = 'VISUAL_EFFECT_DIESEL')
 
 consist.add_unit(weight = 10,
-                capacity = 15,
-                vehicle_length = 5)
+                capacity = 10,
+                vehicle_length = 4)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
