e9a9f245eccd977586a2ea17955a79fd042bb777
andythenorth <mail@andythenorth.co.uk>
2016-06-24 21:23:26 +0100

    Change: improved milk trucks

--- src/vehicles/flow_edge.py
+++ src/vehicles/flow_edge.py
@@ -2,20 +2,20 @@ import global_constants
 from road_vehicle import RVConsist, EdiblesTanker
 
 consist = RVConsist(vehicle_type = EdiblesTanker,
-                id = 'beerwoods',
-                base_numeric_id = 420,
-                title = 'Beerwoods [Edibles Tanker Truck]',
+                id = 'flow_edge',
+                base_numeric_id = 930,
+                title = 'Flow Edge [Edibles Tanker Truck]',
                 replacement_id = '-none',
                 vehicle_life = 40,
-                intro_date = 1943)
+                intro_date = 1912)
 
 consist.add_unit(weight = 12,
-                capacity = 15,
+                capacity = 12,
                 vehicle_length = 5,
                 visual_effect = 'VISUAL_EFFECT_DIESEL')
 
 consist.add_unit(weight = 7,
-                capacity = 15,
+                capacity = 12,
                 vehicle_length = 4)
 
 consist.add_model_variant(intro_date=0,
