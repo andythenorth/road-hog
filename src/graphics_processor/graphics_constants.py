6b0389b50b6a294269bdf17f957f63870a4af5a6
andythenorth <mail@andythenorth.co.uk>
2016-07-09 14:30:00 +0100

    Change: paper cargo support for flatbed trucks, also mask tweaks to some trucks

--- src/graphics_processor/graphics_constants.py
+++ src/graphics_processor/graphics_constants.py
@@ -31,6 +31,6 @@ bulk_cargo_recolour_maps = (("GRVL", {170: 6, 171: 4, 172: 7, 173: 8, 174: 21, 1
                             ("PHOS", {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}))
 
 # Piece
-piece_cargo_maps = ('WOOD', 'WDPR')
+piece_cargo_maps = ('WOOD', 'WDPR', 'PAPR')
 
 # --- End Cargo Maps --- #
