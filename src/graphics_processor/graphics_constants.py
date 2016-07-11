23e2af1b0c012662eecd635d68599eb4f075308c
andythenorth <mail@andythenorth.co.uk>
2016-07-11 21:30:27 +0100

    Feature: crates cargo sprites for goods

--- src/graphics_processor/graphics_constants.py
+++ src/graphics_processor/graphics_constants.py
@@ -31,7 +31,8 @@ bulk_cargo_recolour_maps = (("GRVL", {170: 6, 171: 4, 172: 7, 173: 8, 174: 21, 1
                             ("PHOS", {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}))
 
 # Piece
-piece_cargo_maps = (('WOOD', ['logs']),
+piece_cargo_maps = (('GOOD', ['crates_1']),
+                    ('WOOD', ['logs']),
                     ('WDPR', ['lumber_planks']),
                     ('PAPR', ['paper_coils']))
 
