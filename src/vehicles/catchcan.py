98c29a529fc9358070352640895417fbf05e0d5f
andythenorth <mail@andythenorth.co.uk>
2016-07-17 18:25:41 +0100

    Fix: Catchcan *does* have to be two separate vehicles, not a repeat, because it's asymmetrical

--- src/vehicles/catchcan.py
+++ src/vehicles/catchcan.py
@@ -12,8 +12,12 @@ consist = Tanker(id = 'catchcan',
 consist.add_unit(weight = 16,
                 capacity = 30,
                 vehicle_length = 6,
-                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
-                repeat = 2)
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
+
+consist.add_unit(weight = 16,
+                capacity = 30,
+                vehicle_length = 6,
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
