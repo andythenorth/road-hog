eb2f751f1b79bec1ff6a03f9385fdc9b77e9de61
andythenorth <mail@andythenorth.co.uk>
2016-07-17 11:39:27 +0100

    Change: reworked Colbiggan box tram

--- src/vehicles/colbiggan.py
+++ src/vehicles/colbiggan.py
@@ -10,14 +10,14 @@ consist = BoxHauler(id = 'colbiggan',
                 intro_date = 1940)
 
 consist.add_unit(weight = 12,
-                capacity = 0,
-                vehicle_length = 3,
+                capacity = 48,
+                vehicle_length = 8,
                 effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
 consist.add_unit(weight = 4,
                 capacity = 24,
                 vehicle_length = 4,
-                repeat=4)
+                repeat = 2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
