7f3ac86fd4e6beee3252cab5f9945a3ea0d3d7a9
andythenorth <mail@andythenorth.co.uk>
2016-07-17 18:02:56 +0100

    Change: (WIP) try 4/8 engine for Drumbreck, also adjust num vehicles

--- src/vehicles/drumbreck.py
+++ src/vehicles/drumbreck.py
@@ -10,14 +10,14 @@ consist = Tanker(id = 'drumbreck',
                 intro_date = 1870)
 
 consist.add_unit(weight = 10,
-                vehicle_length = 3,
+                vehicle_length = 4,
                 effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
                 always_use_same_spriterow = True)
 
 consist.add_unit(weight = 5,
-                capacity = 24,
+                capacity = 16,
                 vehicle_length = 4,
-                repeat = 2)
+                repeat = 3)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
