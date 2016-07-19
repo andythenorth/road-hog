b4628da3a54aee6857d0eb890c8705bf2a79c458
andythenorth <mail@andythenorth.co.uk>
2016-07-19 17:24:51 +0100

    Change: box motor sprites for Rakeway box tram

--- src/vehicles/rakeway.py
+++ src/vehicles/rakeway.py
@@ -9,15 +9,11 @@ consist = BoxHauler(id = 'rakeway',
                 vehicle_life = 40,
                 intro_date = 1900)
 
-consist.add_unit(weight = 12,
-                capacity = 0,
-                vehicle_length = 3,
-                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
-
-consist.add_unit(weight = 4,
-                capacity = 20,
-                vehicle_length = 4,
-                repeat = 3)
+consist.add_unit(weight = 16,
+                capacity = 30,
+                vehicle_length = 8,
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
+                repeat = 2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
