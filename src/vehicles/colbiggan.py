1ca2d7ace78c77e96da164d20009a6b9659be893
andythenorth <mail@andythenorth.co.uk>
2016-07-19 18:00:26 +0100

    Change: improved — sprites for Colbiggan

--- src/vehicles/colbiggan.py
+++ src/vehicles/colbiggan.py
@@ -14,10 +14,10 @@ consist.add_unit(weight = 15,
                 vehicle_length = 8,
                 effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
-consist.add_unit(weight = 4,
-                capacity = 18,
-                vehicle_length = 4,
-                repeat = 2)
+consist.add_unit(weight = 15,
+                capacity = 36,
+                vehicle_length = 8,
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
