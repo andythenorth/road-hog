b50de1743eb2d6642364b69c8bed661a09c5f421
andythenorth <mail@andythenorth.co.uk>
2016-07-20 14:58:04 +0100

    Change: (WIP) reworked Stancliffe flatbed tram

--- src/vehicles/stancliffe.py
+++ src/vehicles/stancliffe.py
@@ -10,11 +10,16 @@ consist = FlatBedHauler(id = 'stancliffe',
                 intro_date = 1940)
 
 consist.add_unit(weight = 14,
-                capacity = 30,
+                capacity = 36,
                 vehicle_length = 8,
                 cargo_length = 3,
-                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
-                repeat = 2)
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
+
+consist.add_unit(weight = 14,
+                capacity = 36,
+                vehicle_length = 8,
+                cargo_length = 3,
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
