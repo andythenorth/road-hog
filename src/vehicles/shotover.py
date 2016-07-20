91c5964b0adb1e2900d976482e448f6145829f1f
andythenorth <mail@andythenorth.co.uk>
2016-07-20 15:18:46 +0100

    Change: swap Trotalong livestock tram to use motor sprites, not loco + wagons

--- src/vehicles/shotover.py
+++ src/vehicles/shotover.py
@@ -13,7 +13,7 @@ consist.add_unit(weight = 12,
                 capacity = 36,
                 vehicle_length = 8,
                 effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
-                repeat=2)
+                repeat = 2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
