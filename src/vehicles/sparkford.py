db75efe647e87a9ec2d00cbf47f010d8a6f3d26e
andythenorth <mail@andythenorth.co.uk>
2016-07-21 22:33:56 +0100

    Change: improved - views for multiple trams

--- src/vehicles/sparkford.py
+++ src/vehicles/sparkford.py
@@ -12,8 +12,12 @@ consist = RefrigeratedHauler(id = 'sparkford',
 consist.add_unit(weight = 18,
                 capacity = 36,
                 vehicle_length = 8,
-                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'],
-                repeat = 2)
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
+
+consist.add_unit(weight = 18,
+                capacity = 36,
+                vehicle_length = 8,
+                effects = ['EFFECT_SPRITE_ELECTRIC, 0, 0, 10'])
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
