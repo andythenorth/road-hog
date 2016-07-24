87ff6c444b67d2e11f85d131f4d7340848149e94
andythenorth <mail@andythenorth.co.uk>
2016-07-24 13:27:19 +0100

    Change: 2cc tarps for generic piece cargos

--- src/graphics_processor/graphics_constants.py
+++ src/graphics_processor/graphics_constants.py
@@ -38,7 +38,7 @@ bulk_cargo_recolour_maps = (("AORE", {170: 42, 171: 123, 172: 74, 173: 125, 174:
 # this groups labels and sprites, but there's no obvious problem with that right now
 # if a label can't share a group of sprites, it can repeat some filenames, that's just inefficient, but works
 # DFLT label is a hack to support cargos with no specific sprites (including unknown cargos), and should not be added to cargo translation table
-piece_cargo_maps = ((['DFLT'], ['tarps_blue_1']), # see note on DFLT above
+piece_cargo_maps = ((['DFLT'], ['tarps_2cc_1']), # see note on DFLT above
                     (['GOOD'], ['crates_1']),
                     (['BEER', 'DYES', 'EOIL', 'MILK', 'OIL_', 'PETR', 'RFPR', 'WATR'], ['barrels_silver']),
                     (['COPR'], ['copper_coils']),
