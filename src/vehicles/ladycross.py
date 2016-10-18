3f4f58c6ead8ece577ea2d0ac1a9f735c9c1226c
andythenorth <mail@andythenorth.co.uk>
2016-10-18 20:05:51 +0100

    Fix: Ladycross tram engine length 3/8 not 4/8

--- src/vehicles/ladycross.py
+++ src/vehicles/ladycross.py
@@ -12,7 +12,7 @@ consist = PaxHauler(id = 'ladycross',
 
 consist.add_unit(weight = 6,
                 capacity = 0,
-                vehicle_length = 4,
+                vehicle_length = 3,
                 effect_spawn_model = 'EFFECT_SPAWN_MODEL_STEAM',
                 effects = ['EFFECT_SPRITE_STEAM, -2, 0, 14'])
 
