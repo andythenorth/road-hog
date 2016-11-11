7d9a6d5f6e23f53a7e42f3bb0572a1445cd4f407
andythenorth <mail@andythenorth.co.uk>
2016-11-11 22:19:52 +0000

    Codechange: it's not really a GraphicsProcessorFactory, it's just a GraphicsProcessor, simplify

--- src/graphics_processor/utils.py
+++ src/graphics_processor/utils.py
@@ -2,11 +2,10 @@ from graphics_processor import graphics_constants
 from graphics_processor import pipelines
 
 
-class GraphicsProcessorFactory(object):
-    # simple class which wraps graphics_processor, which uses pixa library
-    # pipeline_name refers to a pipeline class which defines how the processing is done
-    # may be reused across consists, so don't store consist info in the pipeline, pass it to pipeline at render time
-    # this is kind of factory-pattern-ish, but don't make too much of that, it's not important
+class GraphicsProcessor(object):
+    # simple class which wraps a graphics processing pipeline along with some options
+    # pipeline objects may get reused across consists,
+    # so we don't store consist info in the pipeline, it's passed to pipeline.render() method when needed
     def __init__(self, pipeline_name, options):
         self.pipeline_name = pipeline_name
         self.options = options
@@ -18,6 +17,6 @@ def get_composited_cargo_processors(template):
     # also provides optional 2CC recolor
     graphics_options_1 = {'template': template, 'swap_company_colours': False}
     graphics_options_2 = {'template': template, 'swap_company_colours': True}
-    graphics_processor_1 = GraphicsProcessorFactory('extend_spriterows_for_composited_cargos_pipeline', graphics_options_1)
-    graphics_processor_2 = GraphicsProcessorFactory('extend_spriterows_for_composited_cargos_pipeline', graphics_options_2)
+    graphics_processor_1 = GraphicsProcessor('extend_spriterows_for_composited_cargos_pipeline', graphics_options_1)
+    graphics_processor_2 = GraphicsProcessor('extend_spriterows_for_composited_cargos_pipeline', graphics_options_2)
     return (graphics_processor_1, graphics_processor_2)
