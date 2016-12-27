85f8a91b08a609ccdad5ecdd58b4f712ab339b21
andythenorth <mail@andythenorth.co.uk>
2016-12-27 10:11:18 +0000

    Codechange: delete unused dubious metadata caching strategy

--- src/render_nml.py
+++ src/render_nml.py
@@ -30,24 +30,10 @@ generated_nml_path = os.path.join(road_hog.generated_files_path, 'nml')
 if not os.path.exists(generated_nml_path):
     os.mkdir(generated_nml_path)
 
-# we cache some nml metadata to see if we can render templated vehicles faster
-nml_metadata_cache_path = os.path.join('generated', 'nml_metadata.cache')
-if not os.path.exists(nml_metadata_cache_path):
-    codecs.open(nml_metadata_cache_path,'w','utf8').close()
-# this is fragile, playing one line python is silly :)
-module_timestamps = dict((line.split('||',1)[0].strip(), line.split('||',1)[1].strip()) for line in codecs.open(nml_metadata_cache_path,'r','utf8').readlines())
-
 def render_consist_nml(consist):
-    # some slightly dodgy optimisation here
-    vehicle_dirty = True
-    if repo_vars.get('incremental_compile', None) == 'True':
-        if (float(module_timestamps.get(consist.vehicle_module_path, 0)) == os.stat(consist.vehicle_module_path).st_mtime):
-            vehicle_dirty = False
-
-    if vehicle_dirty == True:
-        consist_nml = codecs.open(os.path.join('generated', 'nml', consist.id + '.nml'),'w','utf8')
-        consist_nml.write(utils.unescape_chameleon_output(consist.render()))
-        consist_nml.close()
+    consist_nml = codecs.open(os.path.join('generated', 'nml', consist.id + '.nml'),'w','utf8')
+    consist_nml.write(utils.unescape_chameleon_output(consist.render()))
+    consist_nml.close()
 
 def main():
     consists = road_hog.get_consists_in_buy_menu_order()
@@ -68,12 +54,8 @@ def main():
         pool = Pool(processes=num_pool_workers)
         pool.map(render_consist_nml, consists)
 
-    nml_cache = codecs.open(nml_metadata_cache_path, 'w', 'utf8')
-
     for consist in consists:
         # makefile passes 'roster' arg, but there is no python compile support for compiling a single roster yet
-        metadata = consist.vehicle_module_path + '||' + str(os.stat(consist.vehicle_module_path).st_mtime) + '\n'
-        nml_cache.write(metadata)
         consist_nml = codecs.open(os.path.join('generated', 'nml', consist.id + '.nml'),'r','utf8').read()
         grf_nml.write(consist_nml)
 
