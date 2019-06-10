cd4b353665d46dffdecc8d71a0cfc6296627dea7
andythenorth <mail@andythenorth.co.uk>
2019-06-10 08:07:48 +0100

    Codechange: use a base_platform for Broadrock Dump Truck

--- src/base_platforms/heqs.py
+++ src/base_platforms/heqs.py
@@ -2,14 +2,15 @@ from base_platform import BasePlatform
 from road_vehicle import DieselVehicleUnit
 
 
-class SemiTractorTruckBase(BasePlatform):
+class SemiTractorHaulerBase(BasePlatform):
     capacity = 0
     always_use_same_spriterow = True
     unit_is_semi_tractor = True
 
 
-class DieselConventionalCabSemiTractorTruckGen3A(SemiTractorTruckBase):
+class DieselConventionalCabSemiTractorHaulerGen3A(SemiTractorHaulerBase):
     type = DieselVehicleUnit
     vehicle_length = 2
-    semi_truck_shift_offset_jank = 2
-    effects = ['EFFECT_SPRITE_DIESEL, -2, 1, 10']
+    semi_truck_shift_offset_jank = 3
+    effects = ['EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, 1, 10',
+               'EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, -1, 10']
--- src/vehicles/broadrock_dump.py
+++ src/vehicles/broadrock_dump.py
@@ -1,21 +1,16 @@
 from road_vehicle import DumpHEQSConsist, DieselVehicleUnit
+from base_platforms.heqs import DieselConventionalCabSemiTractorHaulerGen3A
 
 consist = DumpHEQSConsist(id='broadrock_dump',
                    base_numeric_id=100,
                    name='Broadrock',
                    power=400,
-                   #unit_is_semi_tractor=True,
                    speed=40,  # dibbled up above RL for game balance
                    type_base_running_cost_points=20,  # dibble running costs for game balance
                    gen=3,
                    intro_date_offset=-3)  # introduce earlier than gen epoch by design
 
-consist.add_unit(type=DieselVehicleUnit,
-                 vehicle_length=2,
-                 semi_truck_shift_offset_jank=3,
-                 effects=['EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, 1, 10',
-                          'EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE, -2, -1, 10'],
-                 always_use_same_spriterow=True)
+consist.add_unit(base_platform=DieselConventionalCabSemiTractorHaulerGen3A)
 
 consist.add_unit(#capacity=55,  # much bigger is not much better here
                  vehicle_length=6)
