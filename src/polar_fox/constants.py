e47c9ba22e961dd84260662991358dbca31705b9
andythenorth <mail@andythenorth.co.uk>
2019-10-06 08:03:00 +0100

    Codechange: updated Polar Fox

--- src/polar_fox/constants.py
+++ src/polar_fox/constants.py
@@ -92,7 +92,13 @@ cargo_labels = ['PASS', # pax first
                 'STAL',
                 'STCB',
                 'STST',
-                'CAST',
+                'CSTI',
+                'PEAT',
+                'ZINC',
+                'TYRE',
+                'VBOD',
+                'VENG',
+                'NH3_',
                 #
                 'NULL']
 
@@ -128,7 +134,8 @@ allowed_refits_by_label = {'edible_liquids': ['MILK', 'WATR', 'BEER', 'FOOD', 'E
                            'covered_hoppers': ['GRAI', 'WHEA', 'MAIZ', 'SUGR', 'FMSP', 'RFPR', 'CLAY', 'BDMT',
                                                'BEAN', 'NITR', 'RUBR', 'SAND', 'POTA', 'QLME', 'SASH', 'CMNT',
                                                'KAOL', 'FERT', 'SALT', 'CBLK', 'PLAS'],
-                           'cryo_gases': ['CHLO', 'O2__'],
+                           'cryo_gases': ['CHLO', 'O2__', 'NH3_'],
+                           'reefer': ['FOOD', 'FRUT', 'FISH'], # hax for intermodal container sprite selection - reefer car refits work just fine using CC_REFRIGERATED
                            'fruit_veg': ['FRUT', 'BEAN', 'CASS', 'JAVA', 'NUTS'],
                            'cold_metal': ['STEL', 'METL', 'STCB', 'STAL', 'STST', 'COPR']}
 
@@ -150,25 +157,26 @@ disallowed_refits_by_label = {'non_dump_bulk': ['WOOD', 'SGCN', 'FICR', 'BDMT', 
 default_cargos = {'box': ['GOOD', 'VPTS', 'FOOD'],
                   'chemicals_tank': ['ACID', 'RFPR', 'CHLO'],
                   'coal_hopper': ['COAL', 'COKE', 'NITR', 'KAOL'],
-                  'cryo_gases': ['O2__', 'CHLO'],
+                  'cryo_gases': ['O2__', 'CHLO', 'NH3_'],
                   'covered_hopper': ['GRAI', 'KAOL'],
                   'dump': ['IORE', 'MNO2', 'NITR', 'PHOS'],
+                  'dump_high_sides': ['COKE', 'SCMT', 'PEAT', 'COAL'],
                   'edibles_tank': ['WATR', 'MILK', 'BEER'],
                   'express': ['ENSP', 'FMSP', 'GOOD', 'FOOD', 'MAIL'],
                   'flat': ['STEL', 'STCB', 'COPR', 'METL'],
                   'fruit_veg': ['FRUT', 'BEAN', 'CASS', 'JAVA', 'NUTS'],
                   'hopper': ['LIME', 'GRVL', 'SAND', 'PORE', 'SALT', 'IORE', 'CORE'],
-                  # no intermodal, uses box
+                  # intermodal, uses box
                   'silo': ['CMNT', 'BDMT', 'RFPR', 'QLME', 'FMSP'],
                   'stake': ['WOOD'],
                   'mail': ['MAIL'],
                   'metal': ['STEL', 'COPR'],
                   'open': ['GOOD'],
                   'pax': ['PASS'],
+                  'plate': ['IRON', 'CSTI', 'ZINC', 'METL'],
                   'reefer': ['FOOD'],
                   'supplies': ['ENSP'],
                   'tank': ['OIL_', 'CTAR', 'KAOL', 'RUBR'],
-
                   }
 
 
@@ -281,24 +289,26 @@ piece_sprites_to_cargo_labels_maps = {'barrels_silver': ['BEER', 'DYES', 'EOIL',
 
 # Tanker recolour maps
 # DFLT label is a hack to support cargos with no specific sprites (including unknown cargos), and should not be added to cargo translation table
-tanker_livery_recolour_maps = (("OIL_", {136: 1, 137: 2, 138: 3, 139: 4,
-                                         140: 5, 141: 6, 142: 7, 143: 8}),
-                               ("CTAR", {136: 1, 137: 2, 138: 3, 139: 4,
-                                         140: 5, 141: 6, 142: 7, 143: 8}),
-                               # see note on DFLT above
-                               ("DFLT", {136: 198, 137: 199, 138: 200, 139: 201,
-                                         140: 202, 141: 203, 142: 204, 143: 205}),
-                               ("SULP", {136: 62, 137: 63, 138: 64, 139: 65,
-                                         140: 66, 141: 67, 142: 68, 143: 69}),
-                               ("CHLO", {136: 154, 137: 155, 138: 156, 139: 157,
-                                         140: 158, 141: 159, 142: 160, 143: 161}),
-                               # RFPR deliberately 2CC to allow combining with 1CC livery details
-                               ("RFPR", {136: 80, 137: 81, 138: 82, 139: 83,
-                                         140: 84, 141: 85, 142: 86, 143: 87}),
-                               ("RUBR", {136: 40, 137: 41, 138: 42, 139: 43,
-                                         140: 44, 141: 45, 142: 46, 143: 47}),
-                               ("PETR", {136: 16, 137: 17, 138: 18, 139: 19,
-                                         140: 20, 141: 21, 142: 22, 143: 23}))
+# the extended format includes information for intermodal that isn't needed in common cases
+# this is parsed and the intermodal information is dropped for tanker_livery_recolour_maps
+tanker_livery_recolour_maps_extended = (("OIL_", "1CC", # second value is body recolour map for intermodal bulk containers
+                                        {136: 1, 137: 2, 138: 3, 139: 4, 140: 5, 141: 6, 142: 7, 143: 8}),
+                                       ("CTAR", "1CC",
+                                        {136: 1, 137: 2, 138: 3, 139: 4, 140: 5, 141: 6, 142: 7, 143: 8}),
+                                       # see note on DFLT above
+                                       ("DFLT",  "2CC",
+                                        {136: 198, 137: 199, 138: 200, 139: 201, 140: 202, 141: 203, 142: 204, 143: 205}),
+                                       ("SULP",  "1CC",
+                                        {136: 62, 137: 63, 138: 64, 139: 65, 140: 66, 141: 67, 142: 68, 143: 69}),
+                                       # RFPR deliberately 2CC to allow combining with 1CC livery details
+                                       ("RFPR",  "1CC",
+                                        {136: 80, 137: 81, 138: 82, 139: 83, 140: 84, 141: 85, 142: 86, 143: 87}),
+                                       ("RUBR",  "1CC",
+                                        {136: 40, 137: 41, 138: 42, 139: 43, 140: 44, 141: 45, 142: 46, 143: 47}),
+                                       ("PETR", "1CC",
+                                        {136: 16, 137: 17, 138: 18, 139: 19, 140: 20, 141: 21, 142: 22, 143: 23}))
+
+tanker_livery_recolour_maps = [(i[0], i[2]) for i in tanker_livery_recolour_maps_extended]
 
 tanker_livery_recolour_maps = [
     (i[0], i[2]) for i in tanker_livery_recolour_maps_extended
@@ -315,25 +325,68 @@ tanker_livery_recolour_maps_containers = [
 # the extended format includes information for intermodal that isn't needed in common cases
 # this is parsed and the intermodal information is dropped for bulk_cargo_recolour_maps
 # SCMT *is* bulk cargo in this set, realism is not relevant here, went back and forth on this a few times :P
-bulk_cargo_recolour_maps = (("AORE", {170: 42, 171: 123, 172: 74, 173: 125, 174: 162, 175: 126, 176: 78}),
-                            ("CASS", {170: 53, 171: 54, 172: 55, 173: 56, 174: 57, 175: 58, 176: 59}),
-                            ("CLAY", {170: 55, 171: 56, 172: 57, 173: 77, 174: 78, 175: 79, 176: 80}),
-                            ("COAL", {170: 1, 171: 1, 172: 2, 173: 2, 174: 3, 175: 4, 176: 5}),
-                            ("COKE", {170: 1, 171: 1, 172: 2, 173: 2, 174: 3, 175: 4, 176: 5}),
-                            ("CORE", {170: 1, 171: 32, 172: 25, 173: 27, 174: 34, 175: 56, 176: 59}),
-                            ("GRVL", {170: 6, 171: 4, 172: 7, 173: 8, 174: 21, 175: 11, 176: 12}),
-                            ("IORE", {170: 75, 171: 76, 172: 123, 173: 122, 174: 124, 175: 74, 176: 104}),
-                            ("LIME", {170: 35, 171: 58, 172: 36, 173: 37, 174: 38, 175: 38, 176: 39}),
-                            ("KAOL", {170: 11, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
-                            ("MNO2", {170: 1, 171: 16, 172: 3, 173: 17, 174: 18, 175: 19, 176: 20}),
-                            ("NITR", {170: 37, 171: 38, 172: 38, 173: 39, 174: 39, 175: 69, 176: 69}),
-                            ("PHOS", {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}),
-                            ("PORE", {170: 40, 171: 72, 172: 73, 173: 33, 174: 33, 175: 63, 176: 63}),
-                            ("POTA", {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}),
-                            ("SALT", {170: 58, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
-                            ("SAND", {170: 108, 171: 64, 172: 65, 173: 197, 174: 36, 175: 196, 176: 197}),
-                            ("SASH", {170: 11, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
-                            ("SCMT", {170: 104, 171: 3, 172: 2, 173: 70, 174: 71, 175: 72, 176: 3}),
-                            ("SGBT", {170: 60, 171: 53, 172: 54, 173: 55, 174: 56, 175: 57, 176: 58}),
-                            ("SLAG", {170: 24, 171: 3, 172: 2, 173: 3, 174: 4, 175: 5, 176: 5}),
-                            ("SULP", {170: 65, 171: 67, 172: 66, 173: 67, 174: 68, 175: 69, 176: 69}))
+bulk_cargo_recolour_maps_extended = (("AORE", "1CC", # second value is body recolour map for intermodal bulk containers
+                                      {170: 42, 171: 123, 172: 74, 173: 125, 174: 162, 175: 126, 176: 78}),
+                                     ("CASS", "1CC",
+                                      {170: 53, 171: 54, 172: 55, 173: 56, 174: 57, 175: 58, 176: 59}),
+                                     ("CLAY", "1CC",
+                                      {170: 55, 171: 56, 172: 57, 173: 77, 174: 78, 175: 79, 176: 80}),
+                                     ("COAL", "grey",
+                                      {170: 1, 171: 1, 172: 2, 173: 2, 174: 3, 175: 4, 176: 5}),
+                                     ("COKE", "red",
+                                      {170: 1, 171: 1, 172: 2, 173: 2, 174: 3, 175: 4, 176: 5}),
+                                     ("CORE", "2CC",
+                                      {170: 1, 171: 32, 172: 25, 173: 27, 174: 34, 175: 56, 176: 59}),
+                                     ("GRVL", "1CC",
+                                      {170: 6, 171: 4, 172: 7, 173: 8, 174: 21, 175: 11, 176: 12}),
+                                     ("IORE", "1CC",
+                                      {170: 75, 171: 76, 172: 123, 173: 122, 174: 124, 175: 74, 176: 104}),
+                                     ("LIME", "1CC",
+                                      {170: 35, 171: 58, 172: 36, 173: 37, 174: 38, 175: 38, 176: 39}),
+                                     ("KAOL", "1CC",
+                                      {170: 11, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
+                                     ("MNO2", "1CC",
+                                      {170: 1, 171: 16, 172: 3, 173: 17, 174: 18, 175: 19, 176: 20}),
+                                     ("NITR", "1CC",
+                                      {170: 37, 171: 38, 172: 38, 173: 39, 174: 39, 175: 69, 176: 69}),
+                                     ("PHOS", "1CC",
+                                      {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}),
+                                     ("PORE", "1CC",
+                                      {170: 40, 171: 72, 172: 73, 173: 33, 174: 33, 175: 63, 176: 63}),
+                                     ("POTA", "1CC",
+                                      {170: 63, 171: 64, 172: 192, 173: 65, 174: 193, 175: 64, 176: 194}),
+                                     ("SALT", "red",
+                                      {170: 58, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
+                                     ("SAND", "2CC",
+                                      {170: 108, 171: 64, 172: 65, 173: 197, 174: 36, 175: 196, 176: 197}),
+                                     ("SASH", "1CC",
+                                      {170: 11, 171: 12, 172: 13, 173: 14, 174: 14, 175: 15, 176: 15}),
+                                     ("SCMT", "1CC",
+                                      {170: 104, 171: 3, 172: 2, 173: 70, 174: 71, 175: 72, 176: 3}),
+                                     ("SGBT", "1CC",
+                                      {170: 60, 171: 53, 172: 54, 173: 55, 174: 56, 175: 57, 176: 58}),
+                                     ("SLAG", "1CC",
+                                      {170: 24, 171: 3, 172: 2, 173: 3, 174: 4, 175: 5, 176: 5}),
+                                     ("SULP", "1CC",
+                                      {170: 65, 171: 67, 172: 66, 173: 67, 174: 68, 175: 69, 176: 69}))
+
+bulk_cargo_recolour_maps = [(i[0], i[2]) for i in bulk_cargo_recolour_maps_extended]
+
+chemicals_tanker_livery_recolour_maps_extended = (("DFLT", "1CC",
+                                                   {136: CC1, 137: CC1+1, 138: CC1+2, 139: CC1+3, 140: CC1+4, 141: CC1+5, 142: CC1+6, 143: CC1+7}),)
+
+chemicals_tanker_livery_recolour_maps = [(i[0], i[2]) for i in chemicals_tanker_livery_recolour_maps_extended]
+
+cryo_tanker_livery_recolour_maps_extended = (("DFLT", "1CC",
+                                              {136: 5, 137: 7, 138: 9, 139: 11, 140: 12, 141: 13, 142: 14, 143: 15}),
+                                             ("CHLO", "1CC",
+                                              {136: 154, 137: 155, 138: 157, 139: 158, 140: 159, 141: 159, 142: 160, 143: 161}),)
+
+cryo_tanker_livery_recolour_maps = [(i[0], i[2]) for i in cryo_tanker_livery_recolour_maps_extended]
+
+curtain_side_livery_recolour_maps_extended = (("DFLT", "1CC", {}),
+                                              ("TYRE", "black", {}),
+                                              ("VENG", "grey", {}),
+                                              ("VPTS", "2CC", {}))
+
+curtain_side_livery_recolour_maps = [(i[0], i[2]) for i in curtain_side_livery_recolour_maps_extended]
